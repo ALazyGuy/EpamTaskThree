6668428320b9ea2926cb935e236c1fde3743b6e7
ALazyGuy <razorprogrammer@gmail.com>
2021-10-28 04:19:31 +0300

    feat: Configured app finally

--- web/src/main/java/com/epam/esm/Application.java
+++ web/src/main/java/com/epam/esm/Application.java
@@ -3,11 +3,11 @@ package com.epam.esm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WebApplication {
+@SpringBootApplication(scanBasePackages = "com.epam.esm")
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
