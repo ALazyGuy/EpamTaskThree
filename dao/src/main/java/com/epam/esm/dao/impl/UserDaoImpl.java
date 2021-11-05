c11f981cf4cd202f28bd255eb9d8842d3165ca86
ALazyGuy <razorprogrammer@gmail.com>
2021-11-06 02:18:12 +0300

    ref: UserDao

--- dao/src/main/java/com/epam/esm/dao/impl/UserDaoImpl.java
+++ dao/src/main/java/com/epam/esm/dao/impl/UserDaoImpl.java
@@ -3,11 +3,9 @@ package com.epam.esm.dao.impl;
 import com.epam.esm.dao.UserDao;
 import com.epam.esm.model.entity.UserEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -25,39 +23,26 @@ public class UserDaoImpl implements UserDao {
                 existing.setUsername(userEntity.getUsername());
                 existing.setOrderEntities(userEntity.getOrderEntities());
                 entityManager.merge(existing);
-                entityManager.detach(existing);
                 return existing;
             }
+            userEntity.setId(null);
         }
         entityManager.persist(userEntity);
-        entityManager.flush();
-        entityManager.detach(userEntity);
         return userEntity;
     }
 
     @Override
     public Optional<UserEntity> findById(Long id) {
-        UserEntity userEntity = entityManager.find(UserEntity.class, id);
-        if(userEntity == null){
-            return Optional.empty();
-        }
-
-        entityManager.detach(userEntity);
-        return Optional.of(userEntity);
+        return Optional.ofNullable(entityManager.find(UserEntity.class, id));
     }
 
     @Override
     public Optional<UserEntity> findByUsername(String username) {
-        List<UserEntity> userEntities = entityManager
+        return entityManager
                 .createQuery("SELECT user FROM UserEntity user WHERE user.username = ?1")
                 .setParameter(1, username)
-                .getResultList();
-        if(userEntities.size() == 0){
-            return Optional.empty();
-        }
-        UserEntity userEntity = userEntities.get(0);
-        entityManager.detach(userEntity);
-        return Optional.of(userEntity);
+                .getResultStream()
+                .findFirst();
     }
 
     @Override
