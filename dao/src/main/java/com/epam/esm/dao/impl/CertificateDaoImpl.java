62a3c9fef4c0bb7375e1e4c21bc67821a4c01be1
ALazyGuy <razorprogrammer@gmail.com>
2021-11-06 02:13:37 +0300

    ref: CertificateDao

--- dao/src/main/java/com/epam/esm/dao/impl/CertificateDaoImpl.java
+++ dao/src/main/java/com/epam/esm/dao/impl/CertificateDaoImpl.java
@@ -5,10 +5,8 @@ import com.epam.esm.dao.TagDao;
 import com.epam.esm.model.entity.CertificateEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
@@ -35,42 +33,27 @@ public class CertificateDaoImpl implements CertificateDao {
 
     @Override
     public Optional<CertificateEntity> loadByName(String name) {
-        CertificateEntity result;
-        try{
-             result = entityManager
-                    .createQuery("SELECT cert FROM CertificateEntity cert WHERE cert.name = ?1", CertificateEntity.class)
-                    .setParameter(1, name)
-                    .getSingleResult();
-             entityManager.detach(result);
-             return Optional.of(result);
-        }catch (NoResultException noResultException){
-            return Optional.empty();
-        }
+        return entityManager
+                .createQuery("SELECT cert FROM CertificateEntity cert WHERE cert.name = ?1", CertificateEntity.class)
+                .setParameter(1, name)
+                .getResultStream()
+                .findFirst();
     }
 
     @Override
     public CertificateEntity create(CertificateEntity certificateEntity) {
         Optional<CertificateEntity> certificate1 = loadByName(certificateEntity.getName());
         if(certificate1.isPresent()){
-            entityManager.detach(certificate1.get());
             return certificate1.get();
         }
         certificateEntity.getTagEntities().forEach(t -> tagDao.create(t.getName()));
         entityManager.persist(certificateEntity);
-        entityManager.flush();
-        entityManager.detach(certificateEntity);
         return certificateEntity;
     }
 
     @Override
     public Optional<CertificateEntity> loadById(Long id) {
-        CertificateEntity certificateEntity = entityManager.find(CertificateEntity.class, id);
-        if(certificateEntity == null){
-            return Optional.empty();
-        }
-
-        entityManager.detach(certificateEntity);
-        return Optional.of(certificateEntity);
+        return Optional.ofNullable(entityManager.find(CertificateEntity.class, id));
     }
 
     @Override
