2b457c959194376a35a45c7f6f590d21615193f3
ALazyGuy <razorprogrammer@gmail.com>
2021-11-06 04:43:08 +0300

    feat: Implemented tests for tag controller

---
+++ web/src/test/java/com/epam/esm/controller/TagControllerTest.java
@@ -0,0 +1,101 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.Application;
+import com.epam.esm.configuration.ServiceTestConfiguration;
+import com.epam.esm.dao.TagDao;
+import com.epam.esm.model.dto.TagCreateRequest;
+import com.epam.esm.model.dto.TagResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+@Import(ServiceTestConfiguration.class)
+@Transactional
+public class TagControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TagDao tagDao;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @SneakyThrows
+    public void getAllTest(){
+        List<TagResponse> expected = new LinkedList<>();
+        for(int d = 0; d < 10; d++){
+            expected.add(new TagResponse(
+                    tagDao.createIfNotExists(String.format("Tag%d", d))
+            ));
+        }
+
+        String expectedResponse = objectMapper.writeValueAsString(expected);
+        mockMvc.perform(get("/v2/tag/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createBadRequestTest(){
+        TagCreateRequest invalidRequest = new TagCreateRequest();
+        invalidRequest.setName("uyqgewiqwugeiqwe--");
+        mockMvc.perform(
+                post("/v2/tag")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidRequest))
+        ).andExpect(status().is(400));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createConflictTest(){
+        tagDao.createIfNotExists("TESTTAG");
+        TagCreateRequest request = new TagCreateRequest();
+        request.setName("TESTTAG");
+        mockMvc.perform(
+                post("/v2/tag")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isConflict());
+    }
+
+    @Test
+    @SneakyThrows
+    public void createSuccessTest(){
+        TagCreateRequest request = new TagCreateRequest();
+        request.setName("TEST_TAG");
+        MvcResult result = mockMvc.perform(
+                        post("/v2/tag")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                ).andExpect(status().isCreated())
+                .andReturn();
+        TagResponse actual = objectMapper.readValue(result.getResponse().getContentAsString(), TagResponse.class);
+        assertEquals(request.getName(), actual.getName());
+    }
+
+}
